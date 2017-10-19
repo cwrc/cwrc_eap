eff81a1264566a0451c73ce9ff7f8956e2cb90eb
Luke Bainbridge <lbainbridge@echidna.ca>
2017-10-19 14:56:45 -0400

    Materials AJAX as well.

--- js/eap-ajax.js
+++ js/eap-ajax.js
@@ -1,7 +1,7 @@
 (function ($, Drupal) {
   Drupal.behaviors.eapAjax = {
     attach: function (context, settings) {
-      console.log(Drupal.settings);
+
       // Populate associations.
       $('.associations').each(function() {
         // Get component and element for use in success callback.
@@ -25,7 +25,40 @@
                 $(element).find('h2 .badge').text('0');
               }
             },
-            error: function(jqxhr) {}
+            error: function(jqxhr) {
+              $(element).find('h2 .badge').text(Drupal.t('Error'));
+            }
+          });
+        }
+      });
+
+      // Populate materials.
+      $('.materials').each(function() {
+        // Get component and element for use in success callback.
+        var component = $(this).data('component');
+        var element = this;
+
+        // Component must be defined to proceed.
+        if (typeof(component) != 'undefined') {
+          // Lookup data for this component.
+          $.ajax('/islandora/cwrc-eap/material/' + Drupal.settings.cwrc_eap_ajax_pid + '/' + component, {
+            success: function(data) {
+              // Change count on badge and render list.
+              if (typeof(data[component]) != 'undefined') {
+                $('[aria-controls="' + $(element).attr('id') + '"] .badge').text(data[component].length);
+                // $(element).find('h2 .badge').text(data[component].length);
+                var list = $(element).find('ul');
+                for(var i = 0; i < data[component].length; i++) {
+                  var html = data[component][i];
+                  $(list).append('<li>' + html + '</li>');
+                }
+              } else {
+                $('[aria-controls="' + $(element).attr('id') + '"] .badge').text('0');
+              }
+            },
+            error: function(jqxhr) {
+                $('[aria-controls="' + $(element).attr('id') + '"] .badge').text(Drupal.t('Error'));
+            }
           });
         }
       });
